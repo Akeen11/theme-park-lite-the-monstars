2d4d4297a1f3e8b6542f3b5c18e7fcec7e4e723c
Jonathan Edwards <jonathan_edwards@Jonathans-MacBook-Pro.local>
2018-08-01 15:04:31 -0500

    added park areas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 "use strict";
 
 // This is here to get you started. You may remove or move as necessary.
-let parkInfo = require("./parkInfo.js");
+let parkInfo = require("./parkInfo");
+let areaList = require("./areaList")
+
+// areaList();
 
 console.log("park info from main", parkInfo)
 // infoSection.innerHTML = parkInfo;
--- js/parkInfo.js
+++ js/parkInfo.js
@@ -7,8 +7,8 @@ db.fetchParkInfo()
     .then((result) => {
         parkInfo = result
         result.forEach(item => {
-            writeInfo.writeInfo(item.name, item.location, item.hours, item.description);
+            writeInfo.writeInfo(item.name, item.location, item.operating_hours[0].opening, item.operating_hours[0].closing, item.description);
         })
     })
 
-module.exports = parkInfo 
+module.exports = parkInfo   
