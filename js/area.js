2a4251a854f8219dbbfc7096e368f43fb72bb4f1
Jonathan Edwards <jonathan_edwards@Jonathans-MacBook-Pro.local>
2018-08-02 14:13:30 -0500

    Removed undefined bug in areas

--- js/area.js
+++ js/area.js
@@ -1,13 +1,11 @@
 "use strict"
 
-let rideTypes = require("./parkType")
 
-let writeArea = (color, name, description, rideTypes) => {
+let writeArea = (color, name, description) => {
     let area = `
     <div style = "background-color:#${color}" class="text-white m-2 p-3 rounded">
     <h1>${name}</h1>
     <p>${description}</p>
-    <div class="attractionTypes">${rideTypes}</div>
     </div>`;
 
     return area;
